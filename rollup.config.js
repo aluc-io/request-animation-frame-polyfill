e6d6988467c92f92765f4a3d36c80ff4e6ba1e5a
aluc <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2021-02-06 23:35:22 +0900

    dx: change build tool babel to rollup

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/request-animation-frame-polyfill.esm.js',
+      format: 'es',
+    },
+    {
+      file: 'dist/request-animation-frame-polyfill.umd.js',
+      format: 'umd',
+    },
+  ],
+  plugins: [typescript()],
+};
